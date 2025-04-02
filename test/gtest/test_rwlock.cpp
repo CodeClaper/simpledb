4911da724d1267443f7ef2d09ac3bc18bb4855c2
zhouchao03 <jerryzhou.zc@gmail.com>
2025-04-02 22:06:23 +0800

    test: add tests for spinlock and rwolock.

---
+++ test/gtest/test_rwlock.cpp
@@ -0,0 +1,55 @@
+#include "gtest/gtest.h"
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <pthread.h>
+#include <sys/mman.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+extern "C" {
+#include "data.h"
+#include "mmgr.h"
+#include "rwlock.h"
+}
+
+static int *num;
+#define COUNT_NUM 100000
+#define WORKER_NUM 100
+
+static void *work(RWLockEntry *lock) {
+    int i;
+    for (i = 0; i < COUNT_NUM; i++) {
+        AcquireRWlock(lock, RW_WRITER);
+        (*num)++;
+        ReleaseRWlock(lock);
+    }
+    return NULL;
+}
+
+TEST(rwlock, test_rwlock_concurrent) {
+    switch_shared();
+    num = instance(int);
+    *num = 0;
+    RWLockEntry *lock = instance(RWLockEntry);
+    InitRWlock(lock);
+    switch_local();
+
+    int i;
+    for (i = 0; i < WORKER_NUM; i++) {
+        Pid pid  = fork();
+        if (pid < 0) {
+            perror("Bad fork");
+            exit(1);
+        } else if (pid == 0) {
+            work(lock);
+            exit(0);
+        }
+    }
+
+    for (i = 0; i < WORKER_NUM; i++) {
+        wait(NULL);
+    }
+    ASSERT_EQ(10000000, *num);
+}
+
--- test/gtest/test_spinlock.cpp
+++ test/gtest/test_spinlock.cpp
@@ -1,11 +1,36 @@
 #include "gtest/gtest.h"
+#include <cstddef>
+#include <pthread.h>
 
 extern "C" {
+#include "data.h"
+#include "mmgr.h"
 #include "spinlock.h"
 }
 
+static int num = 0;
 static s_lock lock = SPIN_UN_LOCKED_STATUS;
+#define COUNT_NUM 100000
+#define WORKER_NUM 100
 
-TEST(spinlock, ok) {
-    ASSERT_EQ(1, 1);
+static void *work(void *arg) {
+    int i;
+    for (i = 0; i < COUNT_NUM; i++) {
+        acquire_spin_lock(&lock);
+        num++;
+        release_spin_lock(&lock);
+    }
+    return NULL;
+}
+
+TEST(spinlock, test_concurrent) {
+    pthread_t workers[WORKER_NUM];
+    int i;
+    for (i = 0; i < WORKER_NUM; i++) {
+        pthread_create(&workers[i], NULL, work, NULL);
+    }
+    for (i = 0; i < WORKER_NUM; i++) {
+        pthread_join(workers[i], NULL);
+    }
+    ASSERT_EQ(10000000, num);
 }
