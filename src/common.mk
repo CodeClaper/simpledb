bf266e70b41fd823377652beea0162262a54bf5c
zhouchao03 <jerryzhou.zc@gmail.com>
2025-05-27 18:07:18 +0800

    buid: allow print macro when debug.

--- src/common.mk
+++ src/common.mk
@@ -1,6 +1,6 @@
 
 ifdef DEBUG
-	CFLAGS := -g -Wall -O0 $(foreach headerdir, $(headerdirs), -I$(headerdir)) -D DEBUG
+	CFLAGS := -g -Wall -O0 $(foreach headerdir, $(headerdirs), -I$(headerdir)) -g3 -gdwarf-2 -D DEBUG
 else
 	CFLAGS := -O3 $(foreach headerdir, $(headerdirs), -I$(headerdir))
 endif
