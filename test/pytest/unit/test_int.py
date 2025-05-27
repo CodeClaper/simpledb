1400c6c53d4eebd1235b0fc9af548af7e81eb72c
zhouchao03 <jerryzhou.zc@gmail.com>
2025-05-28 05:55:39 +0800

    test: add some tests.

--- test/pytest/unit/test_int.py
+++ test/pytest/unit/test_int.py
@@ -12,15 +12,32 @@ def test_create_table():
 
 
 ## test insert some data.
-def test_insert_some_data():
-    sql = "insert into Score values ('S001', 'Math', 98, 8092);\n"\
-          "insert into Score values ('S002', 'English', 92, 11023);\n"\
-          "insert into Score values ('S003', 'Art', 89, 9920);\n"
+def test_boundary_value():
+    sql = "insert into Score values ('S001', 'Math', 2147483647, 8092);\n"\
+          "insert into Score values ('S002', 'English', -2147483648, 11023);\n"
     ret = client.execute(sql)
     assert_all(ret)
 
+## test query after insert.
+def test_after_inserting():
+    sql = "select * from Score;"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        { "project": "S001", "name": "Math", "score": 2147483647, "total": 8092 },
+        { "project": "S002", "name": "English", "score": -2147483648, "total": 11023 },
+    ]
+
+## test insert exceed int scope value.
 def test_insert_exceed_int_scope():
-    sql = "insert into Score values ('S004', 'Anymouse', 130203202020303030, 99020);"
+    sql = "insert into Score values ('S004', 'Anymouse', 2147483648, 99020);"
+    ret = client.execute(sql)
+    assert ret["success"] == False
+    assert ret["message"] == "Value is int overflow for column 'score'."
+
+
+def test_insert_exceed_nagative_int_scope():
+    sql = "insert into Score values ('S004', 'Anymouse', -2147483649, 99020);"
     ret = client.execute(sql)
     assert ret["success"] == False
     assert ret["message"] == "Value is int overflow for column 'score'."
