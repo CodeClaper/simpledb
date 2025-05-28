cf0f529c5c5a2b5678ab0e45322b95e27ba1b3d3
zhouchao03 <jerryzhou.zc@gmail.com>
2025-05-28 10:09:14 +0800

    test: add some tests.

--- test/pytest/unit/test_int.py
+++ test/pytest/unit/test_int.py
@@ -12,7 +12,7 @@ def test_create_table():
 
 
 ## test insert some data.
-def test_boundary_value():
+def test_boundary_int_value():
     sql = "insert into Score values ('S001', 'Math', 2147483647, 8092);\n"\
           "insert into Score values ('S002', 'English', -2147483648, 11023);\n"
     ret = client.execute(sql)
@@ -30,18 +30,40 @@ def test_after_inserting():
 
 ## test insert exceed int scope value.
 def test_insert_exceed_int_scope():
-    sql = "insert into Score values ('S004', 'Anymouse', 2147483648, 99020);"
+    sql = "insert into Score values ('S003', 'Anymouse', 2147483648, 99020);"
     ret = client.execute(sql)
     assert ret["success"] == False
     assert ret["message"] == "Value is int overflow for column 'score'."
 
 
 def test_insert_exceed_nagative_int_scope():
-    sql = "insert into Score values ('S004', 'Anymouse', -2147483649, 99020);"
+    sql = "insert into Score values ('S003', 'Anymouse', -2147483649, 99020);"
     ret = client.execute(sql)
     assert ret["success"] == False
     assert ret["message"] == "Value is int overflow for column 'score'."
 
+## test insert some data.
+def test_boundary_long_value():
+    sql = f"insert into Score values ('S004', 'Computer', 2147483647, ${(1 << 63) - 1});\n"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
+
+## test query after insert.
+def test_after_inserting_long_value():
+    sql = "select * from Score where project = 'S004';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        { "project": "S004", "name": "Computer", "score": 2147483647, "total": 9223372036854775807 },
+    ]
+
+## test insert some data.
+def test_exceed_long_value():
+    sql = f"insert into Score values ('S004', 'Computer', 2147483647, ${(1 << 63) + 1});\n"
+    ret = client.execute(sql)
+    assert ret["success"] == False
+    assert ret["message"] == "'9223372036854775809' is overflow."
 
 ## test drop table
 def test_drop_table():
