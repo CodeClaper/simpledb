1f35780035be02632c0e5648aacf097d77fcc264
zhouchao03 <jerryzhou.zc@gmail.com>
2025-06-30 19:50:55 +0800

    build: change debug build way.

--- src/common.mk
+++ src/common.mk
@@ -1,6 +1,6 @@
 
 ifdef DEBUG
-	CFLAGS := -g -Wall -O1 $(foreach headerdir, $(headerdirs), -I$(headerdir)) -gdwarf-2 -D DEBUG
+	CFLAGS := -g -Wall $(foreach headerdir, $(headerdirs), -I$(headerdir)) -gdwarf-2 -D DEBUG
 else
 	CFLAGS := -O3 $(foreach headerdir, $(headerdirs), -I$(headerdir))
 endif
