748958b3aed5e8f5069e38310a1c6ced64103c39
zhouchao03 <jerryzhou.zc@gmail.com>
2025-08-28 18:52:52 +0800

    test: add some tests.

--- test/pytest/unit/test_index.py
+++ test/pytest/unit/test_index.py
@@ -18,7 +18,7 @@ def test_insert_data():
         assert ret["success"] == True
 
 ## test index valid
-def test_compare_index_valid():
+def test_compare_index_valid1():
     sql = "select * from Demo where id = '99999';\n"\
           "select * from Demo where sid = '99999';"
     ret = client.execute(sql)
@@ -31,6 +31,7 @@ def test_compare_index_valid2():
     sql = "select count(1) from Demo where id < '99999' and id >= '9999';\n" \
           "select count(1) from Demo where sid < '99999' and sid >= '9999';"
     ret = client.execute(sql)
+    assert_all(ret)
     assert ret[0]["data"] == ret[1]["data"]
     assert ret[0]["duration"] * 10 < ret[1]["duration"]
 
@@ -39,10 +40,63 @@ def test_compare_index_valid3():
     sql = "select count(1) from Demo where id < '111' or id >= '999';\n" \
           "select count(1) from Demo where sid < '111' or sid >= '999';"
     ret = client.execute(sql)
+    assert_all(ret)
     assert ret[0]["data"] == ret[1]["data"]
     assert ret[0]["duration"] * 10 < ret[1]["duration"]
 
+## test critical value
+def test_critical_value1():
+    sql = "select count(1) from Demo where id >= '76150' and id <= '76247';\n" \
+          "select count(1) from Demo where sid >= '76150' and sid <= '76247';"
+    ret = client.execute(sql)
+    assert_all(ret)
+    assert ret[0]["data"] == ret[1]["data"]
+    assert ret[0]["duration"] * 10 < ret[1]["duration"]
 
+## test critical value
+def test_critical_value2():
+    sql = "select count(1) from Demo where id > '75959' and id < '76149';\n" \
+          "select count(1) from Demo where sid > '75959' and sid < '76149';"
+    ret = client.execute(sql)
+    assert_all(ret)
+    assert ret[0]["data"] == ret[1]["data"]
+    assert ret[0]["duration"] * 10 < ret[1]["duration"]
+    
+## test critical value
+def test_critical_value3():
+    sql = "select * from Demo where id = '31180' or id = '144460' or id = '5357' or id = '75862';\n" \
+          "select * from Demo where sid = '31180' or sid = '144460' or sid = '5357' or sid = '75862';"
+    ret = client.execute(sql)
+    assert_all(ret)
+    assert ret[0]["data"] == ret[1]["data"]
+    assert ret[0]["duration"] * 10 < ret[1]["duration"]
+
+## test index valid for like predicate
+def test_index_valid_like1():
+    sql = "select * from Demo where id like '%99999%';\n" \
+          "select * from Demo where sid like '%99999%';\n"
+    ret = client.execute(sql)
+    assert_all(ret)
+    assert ret[0]["data"] == ret[1]["data"]
+    assert abs(ret[0]["duration"] - ret[1]["duration"]) < 1
+
+## test index valid for like predicate
+def test_index_valid_like2():
+    sql = "select * from Demo where id like '%99999';\n" \
+          "select * from Demo where sid like '%99999';\n"
+    ret = client.execute(sql)
+    assert_all(ret)
+    assert ret[0]["data"] == ret[1]["data"]
+    assert abs(ret[0]["duration"] - ret[1]["duration"]) < 1
+
+## test index valid for like predicate
+def test_index_valid_like3():
+    sql = "select * from Demo where id like '99999%';\n" \
+          "select * from Demo where sid like '99999%';\n"
+    ret = client.execute(sql)
+    assert_all(ret)
+    assert ret[0]["data"] == ret[1]["data"]
+    assert ret[0]["duration"] * 10 < ret[1]["duration"]
 
 ## test drop tables
 def test_drop_mock_tables():
---
+++ test/pytest/unit/test_like.py
@@ -0,0 +1,106 @@
+# test_index.py
+from support.db_cli import DbClient
+from support.asserts import assert_all
+
+client = DbClient("127.0.0.1", 4083)    
+client.login("root", "Zc120130211")
+
+## test create table
+def test_create_table():
+    sql = "create table Student (id string primary key, name varchar(32), age int, birth date, address string);"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
+
+## insert mock data. 
+def test_insert_mock_data():
+    sql = "insert into Student values('S001', 'kail', 10, '2014-10-03', 'beijing');\n" \
+          "insert into Student values('S002', 'sun', 11, '2013-11-20', 'shanghai');\n" \
+          "insert into Student values('S003', 'ben', 12, '2012-04-23', 'wuhan');\n" \
+          "insert into Student values('S004', 'david', 14, '2010-01-05', 'jinan');\n" \
+          "insert into Student values('S005', 'kunting', 9, '2015-06-23', 'shijiazhuang');\n" \
+          "insert into Student values('S006', 'bob', 9, '2015-07-07', 'guangzhou');\n" \
+          "insert into Student values('S007', 'july', 11, '2013-03-05', 'shenzhen');\n" \
+          "insert into Student values('S008', 'tail', 13, '2011-08-08', 'xian');\n" 
+    ret = client.execute(sql)
+    assert_all(ret)
+
+## test like predicate
+def test_like_predicate_string():
+    sql = "select * from Student where id like '%001';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        { 'id': 'S001', 'name': 'kail', 'age': 10, 'birth': '2014-10-03', 'address': 'beijing' }
+    ]
+
+## test like predicate
+def test_like_predicate_varchar():
+    sql = "select * from Student where name like '%unting';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        { 'id': 'S005', 'name': 'kunting', 'age': 9, 'birth': '2015-06-23', 'address': 'shijiazhuang' }
+    ]
+
+## test like predicate for date typpe
+def test_like_predicate_date():
+    sql = "select * from Student where birth like '2015-%';"
+    ret = client.execute(sql)
+    assert ret["success"] == False
+    assert ret["message"] == "For like predicate, only support string data type."
+
+## test left wildcard.
+def test_like_predicate_left_wildcard():
+    sql = "select * from Student where name like '%ail';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        { 'id': 'S001', 'name': 'kail', 'age': 10, 'birth': '2014-10-03', 'address': 'beijing' },
+        { 'id': 'S008', 'name': 'tail', 'age': 13, 'birth': '2011-08-08', 'address': 'xian' }
+    ]
+
+## test right wildcard.
+def test_like_predicate_right_wildcard():
+    sql = "select * from Student where address like 'sh%';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        {'id': 'S002', 'name': 'sun', 'age': 11, 'birth': '2013-11-20', 'address': 'shanghai'}, 
+        {'id': 'S005', 'name': 'kunting', 'age': 9, 'birth': '2015-06-23', 'address': 'shijiazhuang'}, 
+        {'id': 'S007', 'name': 'july', 'age': 11, 'birth': '2013-03-05', 'address': 'shenzhen'}
+    ]
+
+## test full wildcard.
+def test_like_predicate_full_wildcard():
+    sql = "select * from Student where address like '%ang%';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        {'id': 'S002', 'name': 'sun', 'age': 11, 'birth': '2013-11-20', 'address': 'shanghai'}, 
+        {'id': 'S005', 'name': 'kunting', 'age': 9, 'birth': '2015-06-23', 'address': 'shijiazhuang'}, 
+        {'id': 'S006', 'name': 'bob', 'age': 9, 'birth': '2015-07-07', 'address': 'guangzhou'}
+    ]
+
+## test no wildcard.
+def test_like_predicate_no_wildcard():
+    sql = "select * from Student where address like 'jinan';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == [
+        {'id': 'S004', 'name': 'david', 'age': 14, 'birth': '2010-01-05', 'address': 'jinan'}
+    ]
+
+## test no wildcard.
+def test_like_predicate_error_wildcard():
+    sql = "select * from Student where address like 'an%g';"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+    assert ret["data"] == []
+
+## test drop table.
+def test_drop_table():
+    sql = "drop table Student;"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
