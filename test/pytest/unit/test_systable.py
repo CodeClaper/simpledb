c45b43fcf214a142b9687e001c46a7516d586094
zhouchao03 <jerryzhou.zc@gmail.com>
2025-12-06 00:10:27 +0800

    test: change test case.

--- test/pytest/unit/test_systable.py
+++ test/pytest/unit/test_systable.py
@@ -25,14 +25,17 @@ def test_query_from_systable():
         {'relname': 'Book', 'reltype': 4}, 
         {'relname': 'Book', 'reltype': 5}, 
         {'relname': 'Book', 'reltype': 6}, 
+        {'relname': 'Book', 'reltype': 7}, 
         {'relname': 'Student', 'reltype': 0}, 
         {'relname': 'Student', 'reltype': 4}, 
         {'relname': 'Student', 'reltype': 5}, 
         {'relname': 'Student', 'reltype': 6}, 
+        {'relname': 'Student', 'reltype': 7}, 
         {'relname': 'Class', 'reltype': 0}, 
         {'relname': 'Class', 'reltype': 4},
         {'relname': 'Class', 'reltype': 5},
         {'relname': 'Class', 'reltype': 6}, 
+        {'relname': 'Class', 'reltype': 7} 
     ]
 
 ## test query from systable.
