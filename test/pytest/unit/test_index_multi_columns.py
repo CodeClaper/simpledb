791aa4b9940c52e2e4734c13c641b7d57290d890
zhouchao03 <jerryzhou.zc@gmail.com>
2026-01-06 13:45:27 +0800

    test: add some tests.

---
+++ test/pytest/unit/test_index_multi_columns.py
@@ -0,0 +1,44 @@
+# test_index_multi-columns.py
+from support.db_cli import DbClient
+from support.data_mock import generate_single_student
+import math
+
+client = DbClient("127.0.0.1", 4083)    
+client.login("root", "Zc120130211")
+
+## test create table.
+def test_create_table():
+    sql = "create table Student(id varchar(32) primary key, name varchar(48), age int, grade varchar(16), sex varchar(4), birth date, phone string, address string, createTime timestamp);\n"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
+def test_insert_mock_data():
+    for id in range(1, 100000):
+        student = generate_single_student(id)
+        sql = f"insert into Student values ('{student["id"]}', '{student["name"]}', {student["age"]},  '{student["grade"]}', '{student["sex"]}', '{student["birth"]}', '{student["phone"]}', '{student["address"]}', '{student["createTime"]}');"
+        ret = client.execute(sql)
+        assert ret["success"] == True
+
+
+def test_create_multi_columns_index():
+    sql = "create index my_index on Student(name, phone, address);"
+    ret = client.execute(sql)
+    assert ret["success"] == True
+
+
+def test_show_index():
+    sql = "show index from Student;"
+    ret = client.execute(sql)
+    print(ret)
+    assert ret["success"] == True
+    assert ret["data"] == [
+            {'index_name': 'Student_pri_index', 'table_name': 'Student', 'is_unique': True, 'index_type': 'BTREE', 'columns': 'id'}, 
+            {'index_name': 'my_index', 'table_name': 'Student', 'is_unique': False, 'index_type': 'BTREE', 'columns': 'name,phone,address'}
+        ]
+
+
+## test drop table.
+def test_drop_table():
+    sql = "drop table Student;"
+    ret = client.execute(sql)
+    assert ret["success"] == True
