1409c1a57446384504627ac693ab451aa1e8f9df
zhouchao03 <jerryzhou.zc@gmail.com>
2026-01-07 14:12:59 +0800

    raf: remove usless code.

--- src/index/binsearch.c
+++ src/index/binsearch.c
@@ -483,7 +483,6 @@ static bool BinSearchInternalNodeForBooleanTerm(SelectPlan *select_plan, void *m
             BinSearchInternalNodeForBooleanTerm(select_plan, min_key, max_key, boolean_term->and_boolean_term);
 }
 
-
 static bool BinSearchInternalNodeForSearchCondition(SelectPlan *select_plan, void *min_key, void *max_key, SearchConditionNode *condition) {
     /* If index is invalid, just return true. */
     if (!select_plan->hit_index) return true;
@@ -491,14 +490,6 @@ static bool BinSearchInternalNodeForSearchCondition(SelectPlan *select_plan, voi
         ? BinSearchInternalNodeForBooleanTerm(select_plan, min_key, max_key, condition->boolean_term)
         : BinSearchInternalNodeForBooleanTerm(select_plan, min_key, max_key, condition->boolean_term) || 
             BinSearchInternalNodeForSearchCondition(select_plan, min_key, max_key, condition->or_search_condition);
-}
-
-void *mockData(char *s1, char *s2, char *s3) {
-    void *data = dalloc(48 + 2 + 13 + 2 + 100 + 2);
-    memcpy(data, s1, strlen(s1));
-    memcpy(data + 48 + 2, s2, strlen(s2));
-    memcpy(data + 48 + 2 + 13 + 2, s3, strlen(s3));
-    return data;
 }
 
 /* Bin search under conditon for internal node. */
@@ -519,18 +510,12 @@ static void BinSearchUnderConditionForInternalNode(MetaIndex *meta_index, uint32
     keys_num = BinInternalNodeGetKeysNum(internal_node);
     high_key = BinNodeGetHighKey(internal_node, meta_index->key_len, meta_index->value_len);
 
-    void *mock = mockData("胡刚", "15691709944", "京市福田区新华路957号");
-    int idx = BinInternalNodeFindCellNum(meta_index, internal_node, mock);
-    
     for (i = 0; i < keys_num; i++) {
         /* Check if index column, use index to avoid full text scanning. */
         /* Current internal node cell key as max key, previous cell key as min key, so the the range of values is (min_key, max_key]. */
 
         uint32_t child_page_num;
         void *max_key, *min_key;
-
-        if (i == idx)
-            db_log(DEBUGER, "HIT");
 
         max_key = BinInternalNodeGetCellKey(internal_node, meta_index->key_len, i);
         min_key = (i == 0) ? NULL : BinInternalNodeGetCellKey(internal_node, meta_index->key_len, i - 1);
