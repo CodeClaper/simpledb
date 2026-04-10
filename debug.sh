db9df15d5aa1833fb9b151478a58bbf09e109713
zhouchao03 <jerryzhou.zc@gmail.com>
2026-04-10 10:40:18 +0800

    feat: add debug.sh

---
+++ debug.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Building with DEBUG=1..."
+make DEBUG=1
+if [ $? -ne 0 ]; then
+    echo "Make failed, exiting..."
+    exit 1
+fi
+
+PID=$(pidof simpledb)
+if [ -n "$PID" ]; then
+    echo "Killing existing simpledb (PID: $PID)..."
+    kill -9 $PID
+else
+    echo "No simpledb process running."
+fi
+
+echo "Cleaning up data directory..."
+rm -rf /home/zc/data/*
+
+echo "Starting GDB..."
+gdb src/simpledb
+
