d2888f138f81b4d7388e722de259b777426c70c8
zhouchao03 <jerryzhou.zc@gmail.com>
2026-04-14 10:41:57 +0800

    feat: add macro unreachable.

--- include/common/log.h
+++ include/common/log.h
@@ -1,6 +1,11 @@
 #include "data.h"
 
 #define panic(...) (db_log(PANIC, __VA_ARGS__))
+#define unreachable(ret_val, ...)   \
+    do {                            \
+        db_log(PANIC, __VA_ARGS__); \
+        return(ret_val);            \
+    } while (0)
 
 char *get_stack_message();
 int get_current_log_fdesc();
