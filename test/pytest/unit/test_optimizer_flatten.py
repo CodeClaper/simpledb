26e7483aa6d86161380456db9a13c2979c4c0e14
zhouchao03 <jerryzhou.zc@gmail.com>
2026-04-15 15:51:56 +0800

    test: change tests.

--- test/pytest/unit/test_optimizer_flatten.py
+++ test/pytest/unit/test_optimizer_flatten.py
@@ -19,71 +19,72 @@ def test_complex_sql1():
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"},
-           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-           {"type": "VAR", "op": "LT", "sflag": "none"}], 
-        "sflag": "none"}
+           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]},
+           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+           {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+           {"type": "VAR", "op": "LT"}], 
+        }
 
 def test_complex_sql2():
     ret = client.execute("express select * from Student s, Teacher t where (s.name = 'zhangsan' or t.name = 'Benj' and t.id = 'T001') and (s.sex = 'M' or t.sex = 'M') or t.createTime < '2025-10-10' or t.createTime >= '2020-01-01';")
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "VAR", "op": "LT", "sflag": "none"}, {"type": "VAR", "op": "GE", "sflag": "none"}], 
-      "sflag": "none"}
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "VAR", "op": "LT"}, 
+        {"type": "VAR", "op": "GE"}
+    ]}
 
 def test_complex_sql3():
     ret = client.execute("express select * from Student s, Teacher t where s.age = 10 or (s.grade = 'G1' or (t.age >= 23 AND t.age <=40 AND (t.sex = 'M' or t.sex = 'F'))) ;")
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-       {"type": "VAR", "op": "EQ", "sflag": "none"}, 
-       {"type": "VAR", "op": "EQ", "sflag": "none"}, 
-       {"type": "AND_SET", "children": [{"type": "VAR", "op": "GE", "sflag": "none"}, {"type": "VAR", "op": "LE", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-       {"type": "AND_SET", "children": [{"type": "VAR", "op": "GE", "sflag": "none"}, {"type": "VAR", "op": "LE", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}], 
-    "sflag": "none"}
+       {"type": "VAR", "op": "EQ"}, 
+       {"type": "VAR", "op": "EQ"}, 
+       {"type": "AND_SET", "children": [{"type": "VAR", "op": "GE"}, {"type": "VAR", "op": "LE"}, {"type": "VAR", "op": "EQ"}]}, 
+       {"type": "AND_SET", "children": [{"type": "VAR", "op": "GE"}, {"type": "VAR", "op": "LE"}, {"type": "VAR", "op": "EQ"}]}
+    ]}
 
 def test_complex_sql4():
     ret = client.execute("express select * from Student where not (birth >= '2010-01-01' AND birth <= '2015-12-31' OR sex = 'M');");
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "LT", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "GT", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}], "sflag": "none"}], 
-     "sflag": "none"}
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "LT"}, {"type": "VAR", "op": "NE"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "GT"}, {"type": "VAR", "op": "NE"}]}
+    ]}
 
 def test_complex_sql5():
     ret = client.execute("express select * from Student where (birth >= '2010-01-01' AND birth <= '2015-12-31' OR sex = 'M') is false;");
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "LT", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}], "sflag": "none"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "GT", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}], "sflag": "none"}], 
-         "sflag": "none"}
+            {"type": "AND_SET", "children": [{"type": "VAR", "op": "LT"}, {"type": "VAR", "op": "NE"}]}, 
+            {"type": "AND_SET", "children": [{"type": "VAR", "op": "GT"}, {"type": "VAR", "op": "NE"}]}
+    ]}
 
 def test_complex_sql6():
     ret = client.execute("express select * from Student where not (birth >= '2010-01-01' AND birth <= '2015-12-31' OR sex = 'M') is false;");
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "GE", "sflag": "none"}, {"type": "VAR", "op": "LE", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "VAR", "op": "EQ", "sflag": "none"}], 
-     "sflag": "none"}
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "GE"}, {"type": "VAR", "op": "LE"}]}, 
+        {"type": "VAR", "op": "EQ"}
+    ]}
 
 def test_complex_sql7():
     ret = client.execute("express select * from Student where not (birth >= '2010-01-01' AND birth <= '2015-12-31' OR not sex = 'M');");
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "LT", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "GT", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}], 
-     "sflag": "none"}
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "LT"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "GT"}, {"type": "VAR", "op": "EQ"}]}
+    ]}
 
 # def test_complex_sql8():
 #     ret = client.execute("express select * from Student where not not (birth >= '2010-01-01' AND birth <= '2015-12-31' OR sex = 'M');");
@@ -99,13 +100,13 @@ def test_complex_sql9():
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"},
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "NE", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "VAR", "op": "LT", "sflag": "none"}, 
-        {"type": "VAR", "op": "GE", "sflag": "none"}], 
-     "sflag": "none"}
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "EQ"}]},
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "NE"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "VAR", "op": "LT"}, 
+        {"type": "VAR", "op": "GE"}
+    ]} 
 
 
 def test_complex_sql10():
@@ -113,10 +114,10 @@ def test_complex_sql10():
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "VAR", "op": "EQ", "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "TRUTH_VALUE", "truth": "true", "sflag": "fail"}], "sflag": "none"}, 
-        {"type": "VAR", "op": "NE", "sflag": "none"}], 
-    "sflag": "none"}
+        {"type": "VAR", "op": "EQ"}, 
+        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+        {"type": "VAR", "op": "NE"}
+    ]}
 
 
 ## test drop table.
