d7baa037a457bd8e97d51dba827241543fbe660a
zhouchao03 <jerryzhou.zc@gmail.com>
2026-04-15 15:14:27 +0800

    test: change tests.

--- test/pytest/unit/test_optimizer_simplify.py
+++ test/pytest/unit/test_optimizer_simplify.py
@@ -18,29 +18,19 @@ def test_complex_sql1():
     ret = client.execute("express select * from Student s, Teacher t where (s.name = 'zhangsan' or t.name = 'Benj') and (s.sex = 'M' or t.sex = 'M') or t.createTime < '2025-10-10' or 1 = 1;")
     assert ret["success"] == True
     print(json.dumps(ret))
-    assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-        {"type": "VAR", "op": "LT", "sflag": "none"}, 
-        {"type": "VAR", "op": "EQ", "sflag": "success"}], 
-     "sflag": "one of success"}
+    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "true"}
 
 def test_complex_sql2():
     ret = client.execute("express select * from Student s, Teacher t where (s.name = 'zhangsan' or t.name = 'Benj' and t.id = 'T001' and 1 = 2) and (s.sex = 'M' or t.sex = 'M' or 1 = 1) or t.createTime < '2025-10-10' or t.createTime >= '2020-01-01';")
     assert ret["success"] == True
     print(json.dumps(ret))
     assert ret["data"] == {"type": "OR_SET", "children": [
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "success"}], "sflag": "none"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "fail"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "one of fail"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "fail"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "one of fail"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "fail"}, {"type": "VAR", "op": "EQ", "sflag": "success"}], "sflag": "one of fail"}, 
-            {"type": "VAR", "op": "LT", "sflag": "none"}, 
-            {"type": "VAR", "op": "GE", "sflag": "none"}], 
-       "sflag": "none"}
+            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}, {"type": "VAR", "op": "EQ"}]}, 
+            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ"}]}, 
+            {"type": "VAR", "op": "LT"}, 
+            {"type": "VAR", "op": "GE"}
+        ]}
 
 
 def test_complex_sql3():
@@ -53,46 +43,31 @@ def test_complex_sql4():
     ret = client.execute("express select * from Student s, Teacher t where 1 = 1 and 2 = 2 and 3 != 3;")
     assert ret["success"] == True
     print(json.dumps(ret))
-    assert ret["data"] == {"type": "AND_SET", "children": [
-        {"type": "VAR", "op": "EQ", "sflag": "success"}, 
-        {"type": "VAR", "op": "EQ", "sflag": "success"}, 
-        {"type": "VAR", "op": "NE", "sflag": "fail"}],
-    "sflag": "one of fail"}
+    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "false"}
 
 def test_complex_sql5():
     ret = client.execute("express select * from Student s, Teacher t where 1 = 2 or 2 = 3 or 3 != 3;")
     assert ret["success"] == True
     print(json.dumps(ret))
-    assert ret["data"] == {"type": "OR_SET", "children": [
-        {"type": "VAR", "op": "EQ", "sflag": "fail"}, 
-        {"type": "VAR", "op": "EQ", "sflag": "fail"}, 
-        {"type": "VAR", "op": "NE", "sflag": "fail"}],
-    "sflag": "all fail"}
+    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "false"}
 
 def test_complex_sql6():
     ret = client.execute("express select * from Student s, Teacher t where s.name = 'zhangsan' or t.name = 'Benj' and t.id = 'T001' or true;")
     assert ret["success"] == True
     print(json.dumps(ret))
-    assert ret["data"] == {"type": "OR_SET", "children": [
-            {"type": "VAR", "op": "EQ", "sflag": "none"}, 
-            {"type": "AND_SET", "children": [{"type": "VAR", "op": "EQ", "sflag": "none"}, {"type": "VAR", "op": "EQ", "sflag": "none"}], "sflag": "none"}, 
-            {"type": "TRUTH_VALUE", "truth": "true", "sflag": "success"}], 
-         "sflag": "one of success"}
+    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "true"}
 
 def test_complex_sql7():
     ret = client.execute("express select * from Student where (false);")
     assert ret["success"] == True
     print(json.dumps(ret))
-    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "false", "sflag": "fail"}
+    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "false"}
 
 def test_complex_sql8():
     ret = client.execute("express select * from Student s, Teacher t where (true and true);")
     assert ret["success"] == True
     print(json.dumps(ret))
-    assert ret["data"] == {"type": "AND_SET", "children": [
-        {"type": "TRUTH_VALUE", "truth": "true", "sflag": "fail"}, 
-        {"type": "TRUTH_VALUE", "truth": "true", "sflag": "fail"}], 
-    "sflag": "all success"}
+    assert ret["data"] == {"type": "TRUTH_VALUE", "truth": "true"}
 
 ## test drop table.
 def test_drop_table():
