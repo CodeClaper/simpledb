af7cbab72db4f5ad82533b9efd9b91f94677e50f
zhouchao03 <jerryzhou.zc@gmail.com>
2026-04-17 09:34:53 +0800

    style: code style change.

--- src/socket/socket.c
+++ src/socket/socket.c
@@ -26,8 +26,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
-int Socket(const char *host, int clientPort)
-{
+int Socket(const char *host, int clientPort) {
     int sock;
     unsigned long inaddr;
     struct sockaddr_in ad;
@@ -39,20 +38,17 @@ int Socket(const char *host, int clientPort)
     inaddr = inet_addr(host);
     if (inaddr != INADDR_NONE)
         memcpy(&ad.sin_addr, &inaddr, sizeof(inaddr));
-    else
-    {
+    else {
         hp = gethostbyname(host);
-        if (hp == NULL)
-            return -1;
+        if (hp == NULL) return -1;
         memcpy(&ad.sin_addr, hp->h_addr, hp->h_length);
     }
     ad.sin_port = htons(clientPort);
     
     sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0)
-        return sock;
-    if (connect(sock, (struct sockaddr *)&ad, sizeof(ad)) < 0)
-        return -1;
+    if (sock < 0) return sock;
+    if (connect(sock, (struct sockaddr *)&ad, sizeof(ad)) < 0) return -1;
+
     return sock;
 }
 
